c8c013015514df80cc2ecb5718e81f2dd742500f
kaleabendrias <kaleabendrias1212@gmail.com>
2023-11-10 01:34:42 +0300

    added signin and signup

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,6 +3,7 @@
 // import ContactUs from "./components/ContactUs"
 import Footer from "./components/Footer"
 import NavBar from "./components/NavBar"
+// import SignIn from "./components/SignIn"
 import SignUp from "./components/SignUp"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       {/* <Body /> */}
       {/* <About /> */}
       {/* <ContactUs /> */}
+      {/* <SignUp /> */}
       <SignUp />
       <Footer />
     </>
--- front/src/components/SignIn.jsx
+++ front/src/components/SignIn.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import { FaFacebookF, FaTwitter, FaGoogle } from 'react-icons/fa';
 
-const SignUp = () => {
+const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -14,28 +14,24 @@ const SignUp = () => {
     setPassword(e.target.value);
   };
 
-  const handleConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
+  const handleRememberMeChange = (e) => {
+    setRememberMe(e.target.checked);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (password === confirmPassword) {
-      // Perform sign-up logic here, such as calling an API or storing the data
-      console.log('Form submitted:', { email, password });
-      // Reset the form
-      setEmail('');
-      setPassword('');
-      setConfirmPassword('');
-    } else {
-      alert("Passwords don't match. Please try again.");
-    }
+    // Perform sign-in logic here, such as calling an API or validating the data
+    console.log('Form submitted:', { email, password, rememberMe });
+    // Reset the form
+    setEmail('');
+    setPassword('');
+    setRememberMe(false);
   };
 
   return (
     <div className="container p-3 d-flex flex-column w-50">
       <div className='shadow-lg p-5 rounded-5 my-4'>
-        <h2 className='lead display-4 d-flex justify-content-center mb-4'>Sign Up</h2>
+        <h2 className='lead display-4 d-flex justify-content-center mb-4'>Sign In</h2>
         <div className="mb-4">
           <label htmlFor="form1" className="form-label">Email address</label>
           <input type="email" className="form-control" id="form1" value={email} onChange={handleEmailChange} />
@@ -45,14 +41,20 @@ const SignUp = () => {
           <input type="password" className="form-control" id="form2" value={password} onChange={handlePasswordChange} />
         </div>
 
-        <div className="mb-4">
-          <label htmlFor="form2" className="form-label">Confirm Password</label>
-          <input type="password" className="form-control" value={confirmPassword} onChange={handleConfirmPasswordChange} />
+        <div className="d-flex justify-content-between mx-3 mb-4 align-items-center">
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value={rememberMe} id="flexCheckDefault" onChange={handleRememberMeChange} />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Remember me
+            </label>
+          </div>
+          <a href="!#">Forgot password?</a>
         </div>
 
-        <button className="btn btn-primary btn-lg btn-block mb-4" onClick={handleSubmit}>Sign Up</button>
+        <button className="btn btn-primary btn-lg btn-block mb-4" onClick={handleSubmit}>Sign in</button>
 
         <div className="text-center">
+          <p>Not a member? <a href="#!">Register</a></p>
           <p>or sign up with:</p>
 
           <div className='d-flex justify-content-between mx-auto' style={{ width: '40%' }}>
@@ -75,4 +77,4 @@ const SignUp = () => {
   );
 }
 
-export default SignUp;
+export default SignIn;
