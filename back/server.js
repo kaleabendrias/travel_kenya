b0d140d9a0d2c1fa16618b690b08d13baa12d41f
kaleabendrias <kaleabendrias1212@gmail.com>
2023-12-05 12:02:24 +0300

    Commented out cookieSession middleware

--- back/server.js
+++ back/server.js
@@ -27,13 +27,13 @@ app.use(cookieParser());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cookieSession({
-    name: "session",
-    keys: process.env.SECRET_KEY.split(","), // Split the string into an array of keys
-    httpOnly: false, // key sent only in https
-  })
-);
+// app.use(
+//   cookieSession({
+//     name: "session",
+//     keys: process.env.SECRET_KEY.split(","), // Split the string into an array of keys
+//     httpOnly: false, // key sent only in https
+//   })
+// );
 
 // Configure session middleware
 app.use(
