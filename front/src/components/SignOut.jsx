9033f343a7316da5e75169b9b40d8a546baad1de
kaleabendrias <kaleabendrias1212@gmail.com>
2023-12-05 14:19:48 +0300

    Refactor SignOut component to remove token from local storage and clear cookies

--- front/src/components/SignOut.jsx
+++ front/src/components/SignOut.jsx
@@ -1,13 +1,24 @@
 import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+
 const SignOut = () => {
-    const navigate = useNavigate();
-    function handleSubmit () {
-        localStorage.removeItem('token');
-        navigate('/signin')
-    }
+  const navigate = useNavigate();
+
+  function handleSubmit() {
+    // Remove token from local storage
+    localStorage.removeItem('token');
+
+    // Clear cookies
+    const cookies = Object.keys(Cookies.get());
+    cookies.forEach(cookie => Cookies.remove(cookie));
+    navigate('/signin');
+  }
+
   return (
-    <button className="nav-link text-white justify" onClick={handleSubmit}>SignOut</button>
-  )
+    <button className="nav-link text-white justify" onClick={handleSubmit}>
+      SignOut
+    </button>
+  );
 }
 
 export default SignOut;
